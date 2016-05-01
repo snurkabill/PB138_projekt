7da0b077484b436ba06fbc2687537da4d186649d
Simon Struk <simon.struk@gmail.com>
2016-05-01 21:29:49 +0200

    Basic authentication

--- src/main/java/annotator/UserFilter.java
+++ src/main/java/annotator/UserFilter.java
@@ -4,13 +4,15 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 
 /**
- * TODO: This should prevent unauthorized access to application
+ * This should prevent unauthorized access to application
  * Created by simon on 1.5.16.
  */
-@WebFilter(urlPatterns = {"/Home/*"})
+@WebFilter(urlPatterns = {"/*"})
 public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -23,6 +25,35 @@ public class UserFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+        String auth = request.getHeader("Authorization");
+        if (auth == null) {
+            response401(response);
+            return;
+        }
+
+        String[] creds = parseAuthHeader(auth);
+        String username = creds[0];
+        String password = creds[1];
+
+        /* here goes check with users in mongoDB */
+        if (username.equals("admin") && password.equals("admin")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("authenticatedUser", username);
+            chain.doFilter(request, response);
+        } else {
+            response401(response);
+            return;
+        }
+    }
+
+    private String[] parseAuthHeader(String auth) {
+        return new String(DatatypeConverter.parseBase64Binary(auth.split(" ")[1])).split(":", 2);
+    }
+
+    private void response401(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("WWW-Authenticate", "Basic realm=\"Annotator\"");
+        response.getWriter().println("<html><body><h1>401 Unauthorized</h1></body></html>");
     }
 
     @Override
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,17 +1,18 @@
 <%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
 <html>
 <head>
-    <title>Annotator</title>
+    <title>Annotator - home</title>
 </head>
 <body>
 <h2>Annotator</h2>
 
-    <form action="Login" method="post">
+    <h3>Welcome, ${sessionScope.authenticatedUser} </h3>
+    <a href="profile.jsp">User profile</a> <br/>
 
-        Login:<input type="text" name="uname"/><br/>
-        Password:<input type="password" name="pass"/><br/>
-        <input type="submit" value="Login"/>
+    Here may be a list of packages and upload button
+
+    <form action="Logout" method="post">
+        <input type="submit" value="Logout"/>
     </form>
-
 </body>
 </html>
