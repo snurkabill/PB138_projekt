1389b746d99e63543e08c30faab4b8b9a8074390
Jiří Vahala <snurkabill@gmail.com>
2016-05-10 00:51:31 +0200

    active packate added

---
+++ src/main/java/annotator/model/activepackage/ActivePackageNotFoundException.java
@@ -0,0 +1,8 @@
+package annotator.model.activepackage;
+
+public class ActivePackageNotFoundException extends Exception {
+
+    public ActivePackageNotFoundException(String message) {
+        super(message);
+    }
+}
