07a1b68dccd5169d847db3cfca7b2c02e0d843db
Jiří Vahala <snurkabill@gmail.com>
2016-05-12 22:52:56 +0200

    finished user stats

--- src/main/java/annotator/model/statistics/UserStatistics.java
+++ src/main/java/annotator/model/statistics/UserStatistics.java
@@ -1,26 +1,44 @@
 package annotator.model.statistics;
 
+import annotator.model.vote.Vote;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserStatistics {
 
-    private final Double kappa;
-    private final Double overallAccuracy;
-    private final Double randomAgreementProbability;
+    private final Double trueRatio;
+    private final Double averageDuration;
+    private final Double averageDurationOnNoisyData;
+    private final ConfusionMatrixStatistics confusionMatrix;
 
-    public UserStatistics(Double kappa, Double overallAccuracy, Double randomAgreementProbability) {
-        this.kappa = kappa;
-        this.overallAccuracy = overallAccuracy;
-        this.randomAgreementProbability = randomAgreementProbability;
+    public UserStatistics(List<Vote> noisyVoteList, List<Vote> allVoteList) {
+        this.trueRatio = ((Long) allVoteList.stream()
+                .filter(Vote::getBelongsToType)
+                .count())
+                .doubleValue() / allVoteList.size();
+        this.averageDuration = allVoteList.stream().mapToLong(Vote::getDuration).average().getAsDouble();
+        this.averageDurationOnNoisyData = noisyVoteList.stream().mapToLong(Vote::getDuration).average().getAsDouble();
+
+        confusionMatrix = new ConfusionMatrixStatistics(
+                noisyVoteList.stream().map(Vote::getBelongsToType).collect(Collectors.toList()),
+                new ArrayList<>()); // new ArrayList<>() ~ all values are false.
     }
 
-    public Double getKappa() {
-        return kappa;
+    public Double getTrueRatio() {
+        return trueRatio;
     }
 
-    public Double getOverallAccuracy() {
-        return overallAccuracy;
+    public Double getAverageDuration() {
+        return averageDuration;
     }
 
-    public Double getRandomAgreementProbability() {
-        return randomAgreementProbability;
+    public Double getAverageDurationOnNoisyData() {
+        return averageDurationOnNoisyData;
+    }
+
+    public ConfusionMatrixStatistics getConfusionMatrix() {
+        return confusionMatrix;
     }
 }
