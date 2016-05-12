2298bc2bd874cf0fd6e1c4f96f4f5934e2707e97
Jiří Vahala <snurkabill@gmail.com>
2016-05-12 22:52:56 +0200

    finished user stats

--- src/main/java/annotator/model/statistics/WordStatistics.java
+++ src/main/java/annotator/model/statistics/WordStatistics.java
@@ -1,20 +1,27 @@
 package annotator.model.statistics;
 
+import annotator.model.vote.Vote;
+
+import java.util.List;
+
 public class WordStatistics {
 
     private final Double trueRatio;
-    private final Long averageDuration;
+    private final Double averageDuration;
 
-    public WordStatistics(Double trueRatio, Long averageDuration) {
-        this.trueRatio = trueRatio;
-        this.averageDuration = averageDuration;
+    public WordStatistics(List<Vote> voteList) {
+        this.trueRatio = ((Long) voteList.stream()
+                .filter(Vote::getBelongsToType)
+                .count())
+                .doubleValue() / voteList.size();
+        this.averageDuration = voteList.stream().mapToLong(Vote::getDuration).average().getAsDouble();
     }
 
     public Double getTrueRatio() {
         return trueRatio;
     }
 
-    public Long getAverageDuration() {
+    public Double getAverageDuration() {
         return averageDuration;
     }
 }
