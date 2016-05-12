398e62decfa33381ef739688bea3f2fe40b096c2
Jiří Vahala <snurkabill@gmail.com>
2016-05-12 20:32:38 +0200

    user statistics

---
+++ src/main/java/annotator/statistics/UserStatistics.java
@@ -0,0 +1,26 @@
+package annotator.statistics;
+
+public class UserStatistics {
+
+    private final Double kappa;
+    private final Double overallAccuracy;
+    private final Double randomAgreementProbability;
+
+    public UserStatistics(Double kappa, Double overallAccuracy, Double randomAgreementProbability) {
+        this.kappa = kappa;
+        this.overallAccuracy = overallAccuracy;
+        this.randomAgreementProbability = randomAgreementProbability;
+    }
+
+    public Double getKappa() {
+        return kappa;
+    }
+
+    public Double getOverallAccuracy() {
+        return overallAccuracy;
+    }
+
+    public Double getRandomAgreementProbability() {
+        return randomAgreementProbability;
+    }
+}
