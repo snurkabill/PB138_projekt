52382fcfd46e9895f0fb4b19dc2651653bd288ed
Pavel Kučera <pave.kucera@gmail.com>
2016-05-21 22:20:08 +0200

    remove logic from User entity

--- src/main/java/annotator/model/user/User.java
+++ src/main/java/annotator/model/user/User.java
@@ -13,12 +13,18 @@ public class User {
     private String email;
     private String passwordHash;
 
-    public User(String email, String passwordHash, String id) throws UserNotFoundException {
-        if (id == null)
-            throw new UserNotFoundException("User : id not found");
+    public User(String id, String email, String passwordHash) {
         this.id = id;
         this.email = email;
         this.passwordHash = passwordHash;
+    }
+
+    public User(Document document) {
+        this(
+            document.getObjectId("_id").toString(),
+            document.getString("email"),
+            document.getString("password_hash")
+        );
     }
 
     public String getEmail() {
--- src/main/java/annotator/model/user/UserRepository.java
+++ src/main/java/annotator/model/user/UserRepository.java
@@ -5,7 +5,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
 public class UserRepository extends AbstractRepository {
 
@@ -16,18 +15,23 @@ public class UserRepository extends AbstractRepository {
     }
 
     public User getOneByEmail(String email) throws UserNotFoundException {
-        return convertTo(this.users.find(Filters.eq("email", email)).first());
-    }
+        Document userDocument = this.users.find(Filters.eq(
+            "email",
+            email
+        )).first();
 
-    private static User convertTo(Document document) throws UserNotFoundException {
-        return new User(document.getString("email"), document.getString("passwordHash"), document.get("_id").toString());
+        if (userDocument == null) {
+            throw new UserNotFoundException("User not found: " + email);
+        }
+
+        return new User(userDocument);
     }
 
     public User createUser(String email, String password) throws UserNotFoundException {
-        Document newUser = new Document("_id", new ObjectId())
+        Document userDocument = new Document()
             .append("email", email)
             .append("passwordHash", password);
-        this.users.insertOne(newUser);
-        return convertTo(newUser);
+        this.users.insertOne(userDocument);
+        return new User(userDocument);
     }
 }
