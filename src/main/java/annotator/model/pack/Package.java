18ba0f1ede6744e0585b31e3af26d07f2b75a52b
Pavel Kučera <pave.kucera@gmail.com>
2016-05-22 08:39:16 +0200

    add package name

--- src/main/java/annotator/model/pack/Package.java
+++ src/main/java/annotator/model/pack/Package.java
@@ -8,16 +8,19 @@ public class Package {
 
     private final String id;
     private final String typeId;
+    private final String name;
     private final Integer wordCount;
     private final ArrayList<String> wordList;
 
     public Package(
         String id,
         String typeId,
+        String name,
         ArrayList<String> wordList
     ) {
         this.id = id;
         this.typeId = typeId;
+        this.name = name;
         this.wordCount = wordList.size();
         this.wordList = wordList;
     }
@@ -26,6 +29,7 @@ public class Package {
         this(
             document.getObjectId("_id").toString(),
             document.getString("type_id"),
+            document.getString("name"),
             (ArrayList<String>) document.get("words")
         );
     }
@@ -36,6 +40,10 @@ public class Package {
 
     public String getTypeId() {
         return this.typeId;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public Integer getWordCount() {
