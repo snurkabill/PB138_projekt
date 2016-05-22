424ed26ee2d888dc2406fa76ea135bb75af90857
Pavel Kučera <pavelkucera@users.noreply.github.com>
2016-05-22 10:35:06 +0200

    add very basic controller / template support (#12)

--- src/main/java/annotator/server/Login.java
+++ src/main/java/annotator/server/Login.java
@@ -3,43 +3,41 @@ package annotator.server;
 import annotator.model.user.User;
 import annotator.model.user.UserNotFoundException;
 import annotator.model.user.UserRepository;
-import com.mongodb.client.MongoDatabase;
 import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns={"/Login"})
-public class Login extends HttpServlet {
+public class Login extends Controller {
+
+    private UserRepository userRepository;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void initializeDependencies(ServiceLocator serviceLocator) {
+        this.userRepository = serviceLocator.getUserRepository();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        UserRepository userRepository = new UserRepository(
-            (MongoDatabase) this.getServletContext().getAttribute("database")
-        );
-
         try {
-            User user = userRepository.getOneByEmail(username);
+            User user = this.userRepository.getOneByEmail(username);
             if (!BCrypt.checkpw(password, user.getPasswordHash())) {
                 throw new InvalidPasswordException();
             }
 
-            HttpSession session = request.getSession();
-            session.setAttribute("authenticatedUser", username);
+            this.session.setAttribute("authenticatedUser", username);
             response.sendRedirect("index.jsp");
 
         } catch (UserNotFoundException | InvalidPasswordException e) {
-            request.setAttribute("message", "Invalid username/password");
-            request.getRequestDispatcher("index.jsp").forward(request, response);
+            this.template.set("message", "Invalid username/password");
+            this.render("index.jsp", request, response);
         }
     }
 }
